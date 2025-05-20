6fcdc7d3e691dfa012307c97179c1bce70db1753
mjoaonic <mjoao.a.s.nicolau@gmail.com>
2025-05-20 22:17:31 +0100

    Add local files before pulling remote updates

---
+++ cabecalho.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>cabeçalho</title>
+</head>
+<body>
+
+<div class="logo">
+</div>
+
+<nav>
+	<li><a href="base.php"> MENU INICIAL </li>
+</nav>
+
+</body>
+</html>
